55c1300af8b732381560c707ccdb41c3771be4ac
Smiley <rjchatfield@gmail.com>
2014-01-19 18:01:03 +1000

    Finish static pages exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 AuctionApp::Application.routes.draw do
+
+  root "static_pages#home"
+
+  # get "static_pages/home"
+  match '/home', to: 'static_pages#home', via: 'get'
+  # get "static_pages/about"
+  match '/about', to: 'static_pages#about', via: 'get'
+  # get "static_pages/help"
+  match '/help', to: 'static_pages#help', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
