2ba2cc3a1b55e09baf43fd1534d53219c9969efa
Robert j Chatfield <robert.chatfield@griffithuni.edu.au>
2014-01-19 10:11:27 +1000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+== README
+
+Describe the
+
+* application
+
+* its purpose
+
+* its state
+
+* its usage
+
+* its author
+
+* and so on.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuctionApp::Application.config.secret_key_base = 'b0ca6c5df9674956d13c904864bf7f7d2834a1e60558dfdd3ae4363bd9c09521efc2bffec5f1d1474f8148f376bc0e2181384be067d2c4e8468951b2a68df2a8'
