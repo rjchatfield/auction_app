f60c128c2b30f7478b46e8ed310d31162d8bd461
Smiley <rjchatfield@gmail.com>
2014-01-20 22:27:30 +1000

    Styled everything good

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 AuctionApp::Application.routes.draw do
 
-  root "static_pages#home"
+  root "static_pages#contact"
 
-  # get "static_pages/home"
-  match '/home', to: 'static_pages#home', via: 'get'
+  # get "static_pages/contact"
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # get "static_pages/about"
   match '/about', to: 'static_pages#about', via: 'get'
   # get "static_pages/help"
