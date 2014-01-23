e5ffe897d1ccc7b91d3b2f25e16430753eba2b9c
Robert j Chatfield <robert.chatfield@griffithuni.edu.au>
2014-01-23 13:43:39 +1000

    Add validations to category and item models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   default_scope -> {order('created_at DESC')};
   belongs_to :category
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true, length: 3..50
+  validates :starting_price, numericality: true
 end
