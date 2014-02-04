c8dca40856ba7420651f448df279d2e940e369d1
Robert j Chatfield <robert.chatfield@griffithuni.edu.au>
2014-02-04 23:31:06 +1000

    More image upload tweeks

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ActiveRecord::Base
 
   # Paperclip
   has_attached_file :avatar,
-                    styles: { :medium => "300x300>", :thumb => "50x50>" },
+                    styles: { :medium => "200x200>", :thumb => "50x50>" },
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :avatar, less_than: 3.megabytes
